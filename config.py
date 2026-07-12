22bc35a78e358513ca3b8734597b46a9763f03ad
Georgia-Floyd-19 <ngangageorgia7@gmail.com>
2026-07-12 02:19:03 +0100

    Ajout support st.secrets pour deploiement Streamlit Cloud

--- config.py
+++ config.py
@@ -5,6 +5,15 @@ import sys
 load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI")
+
+if not MONGODB_URI:
+    try:
+        import streamlit as st
+        MONGODB_URI = st.secrets.get("MONGODB_URI")
+        os.environ["DATABASE_NAME"] = st.secrets.get("DATABASE_NAME", "bibliotech")
+    except Exception:
+        pass
+
 if not MONGODB_URI:
     sys.exit("ERREUR: MONGODB_URI non defini. Copie .env.example en .env et configure-le.")
 
